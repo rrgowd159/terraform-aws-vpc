f50e49e5c8bfe742982bef1686e7e0dbcf3f4416
rakeshgowd100 <rakeshgowd100@gmail.com>
2024-11-17 22:56:15 +0530

    test

--- main.tf
+++ main.tf
@@ -1,11 +1,29 @@
 resource "aws_vpc" "vpc" {
-  cidr_block = var.block_cidr
+  cidr_block = var.vpc_cidr
   tags = var.vpc_tags
 }
 
+resource "aws_subnet" "vpc_subnet" {
+  cidr_block = var.subnet_cidr
+  vpc_id = aws_vpc.vpc.id
+  availability_zone = "us-east-1a"
+}
 
-resource "aws_vpc" "vpc_alias" {
-  cidr_block = var.block_cidr_alias
-  tags = var.vpc_tags
-  provider = aws.aws-us-east-1
+resource "aws_internet_gateway" "vpc_igw" {
+  vpc_id = aws_vpc.vpc.id
+}
+
+resource "aws_route_table" "vpc_route_table" {
+  vpc_id = aws_vpc.vpc.id
+}
+
+resource "aws_route" "vpc_route" {
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.vpc_igw.id
+  route_table_id = aws_route_table.vpc_route_table.id
+}
+
+resource "aws_route_table_association" "vpc_subnet_association" {
+  subnet_id = aws_subnet.vpc_subnet.id
+  route_table_id = aws_route_table.vpc_route_table.id
 }
--- variables.tf
+++ variables.tf
@@ -1,19 +1,14 @@
-variable "block_cidr" {
+variable "vpc_cidr" {
   description = "This is cidr block for vpc"
   type = string
-  default = "10.0.0.0/16"
 }
+
 variable "vpc_tags" {
   description = "This is tags for vpc"
   type = map(string)
-  default = {
-    "Name" = "VPC"
-    "Env" = "Test"
-  }
 }
 
-variable "block_cidr_alias" {
+variable "subnet_cidr" {
   description = "This is cidr block for vpc"
   type = string
-  default = "10.0.0.0/16"
 }
